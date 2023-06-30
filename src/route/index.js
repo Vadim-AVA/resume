83e15f8409965e2b201be6080a311982b49a5860
Vadim-AVA <avavadim@ukr.net>
2023-06-30 03:40:08 +0300

    Level 2 Modul 4 lesson 2 end 1 task

--- src/route/index.js
+++ src/route/index.js
@@ -777,10 +777,11 @@ router.get('/js', function (req, res) {
 router.get('/car', function (req, res) {
   // res.render генерує нам HTML сторінку
 
-  layout: 'basic',
+
     
   //            ↙ cюди вводимо назву файлу з сontainer
   res.render('car', {
+    layout: 'basic',
     make: 'Toyota',
     model: 'Camry',
     year: 2022,
@@ -879,9 +880,10 @@ router.get('/car', function (req, res) {
 
 router.get('/mak', function (req, res) {
   // res.render генерує нам HTML сторінку
-  layout: 'basic',
+
   //            ↙ cюди вводимо назву файлу з сontainer
   res.render('mak', {
+    layout: 'basic',
     name: 'Apple MacBook Pro',
     description: 'The ultimate notebook for power users',
     price: 1999.99,
@@ -969,9 +971,10 @@ router.get('/mak', function (req, res) {
 
 router.get('/facebook', function (req, res) {
   // res.render генерує нам HTML сторінку
-  layout: 'basic',
+
   //            ↙ cюди вводимо назву файлу з сontainer
   res.render('facebook', {
+    layout: 'basic',
     name: 'Facebook',
     users: [
       {
@@ -1122,6 +1125,81 @@ router.get('/facebook', function (req, res) {
   })
   //                  ↑↑ сюди вводимо JSON дані
 })
+
+// ================================================================
+
+// router.get Створює нам один ентпоїнт
+
+//           ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/task21', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('task21', {
+    layout: 'bootstrap',
+    heading: {
+      main: {
+        title: 'JavaScript',
+        text: 'JavaScript is a popular high-level, dynamic, and interpreted programming language.',
+        button_text: {
+          solid: 'Open page',
+          outline: 'Add to cart',
+        },
+      },
+      second: {
+        title: 'Stack Overflow',
+        text: 'First-class functions',
+        button_text: {
+          solid: 'Open page',
+          outline: 'Add to cart',
+        },
+      },
+      subblock: {
+        title: 'JavaScript Weekly',
+        text: 'Prototype-based inheritance newsletter',
+        button_text: {
+          solid: 'Open page',
+          outline: 'Add to cart',
+        },
+      },
+    },
+    nested_display: {
+      title: 'HTML',
+      children_display: {
+title        : 'Outdated HTML tags',
+        text: 'tags that have been identified as deprecated and not recommended for use in HTML version 5',
+        button: {
+          text: 'Орen',
+        },
+      },
+      header_text: 'Sections',
+      button: {
+        text: 'Learn more',
+      },
+    },
+    paragraph: {
+      title: 'About modules',
+      text: 'Modules with import/export statements',
+
+      button: {
+        text: 'Open link page',
+        url: 'https://google.com',
+      },
+      list_name: [
+        {
+          text: 'Github',
+          url: 'https://github.com/',
+        },
+        {
+          text: 'Bootstrap',
+          url: 'https://getbootstrap.com/',
+        },
+      ],
+    },
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+
 
 // ================================================================
 
